73e88bde1fbbf58fec742002cc71b9a384e332a6
Zara Martín <zmartand@myuax.es>
2023-03-21 13:35:42 +0100

    Ejercicio 1 terminado

---
+++ src/main/java/Ejercicio1/Main.java
@@ -0,0 +1,8 @@
+package Ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        RaizCuadrada raizCuadrada = new RaizCuadrada();
+        raizCuadrada.CalculadoraRaizCuadrada();
+    }
+}
